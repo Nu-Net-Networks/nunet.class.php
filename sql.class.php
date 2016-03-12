df796594703072d79c861fe59285ee9d5e6f68d3
Seth Rainsdon <srainsdon@nunetnetworks.net>
2016-03-11 21:20:26 -0800

    removed useless stuff from top

--- sql.class.php
+++ sql.class.php
@@ -1,11 +1,5 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * class sql: this is a helper class for building and working with sql
  *
@@ -41,30 +35,30 @@ class sql {
         if ($this->db->connect_error) {
             $this->log->error("Connection failed: " . $this->db->connect_error);
         } else {
-            echo "Connected successfully";
+            $this->log->trace("Connected successfully");
         }
     }
 
     public function createSql($tableName, $dataArray) {
         $this->log->debug("$tableName - " . json_encode($dataArray));
-        $this->connected = @mysqli_ping() ? true : false;
+        $this->connected = @mysqli_ping($this->db) ? true : false;
         $colString = '';
         $valueString = '';
-        if ($this->connected) { // This will not work if there is no db connection but is safer
+        if ($this->connected) {
             $this->log->debug("Connected will use mysql_real_escape_string");
         } else {
             $this->log->warn("Not Connected to a database the escaping of char will not be as secure");
         }
         foreach ($dataArray as $key => $value) {
             $colString .= "`$key`,";
-            //if ($this->connected) { // This will not work if there is no db connection but is safer
-            $valueString .= "'" . mysqli_real_escape_string($this->db, $value) . "',";
-            /* } else { // This will work if there is no db connection
-              $search = array("\\", "\x00", "\n", "\r", "'", '"', "\x1a");
-              $replace = array("\\\\", "\\0", "\\n", "\\r", "\'", '\"', "\\Z");
-              $value = str_replace($search, $replace, $value);
-              $valueString .= "'" . $value . "',";
-              } */
+            if ($this->connected) { // This will not work if there is no db connection but is safer
+                $valueString .= "'" . mysqli_real_escape_string($this->db, $value) . "',";
+            } else { // This will work if there is no db connection
+                $search = array("\\", "\x00", "\n", "\r", "'", '"', "\x1a");
+                $replace = array("\\\\", "\\0", "\\n", "\\r", "\'", '\"', "\\Z");
+                $value = str_replace($search, $replace, $value);
+                $valueString .= "'" . $value . "',";
+            }
         }
         $colString = rtrim($colString, ",");
         $valueString = rtrim($valueString, ",");
